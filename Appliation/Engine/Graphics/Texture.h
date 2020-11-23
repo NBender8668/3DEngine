d117028ccdfb4aa1b0a29c72f56b1bc061a76db2
NBender8668 <61422838+NBender8668@users.noreply.github.com>
2020-11-23 09:55:05 -0700

    Add project files.

---
+++ Appliation/Engine/Graphics/Texture.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../Resources/Resource.h"
+
+namespace nc
+{
+	class Texture : public Resource
+	{
+	public:
+		// Inherited via Resource
+		virtual bool Create(const std::string name, void* data = nullptr) override;
+		virtual void Destory() override;
+		
+		bool CreateTexture(const std::string& filename, GLenum target = GL_TEXTURE_2D, GLuint unit = GL_TEXTURE0);
+
+	protected:
+		GLenum m_target{ GL_TEXTURE_2D };
+		GLuint m_unit{ GL_TEXTURE0 };
+		GLuint m_texture{ 0 };
+
+	};
+}
